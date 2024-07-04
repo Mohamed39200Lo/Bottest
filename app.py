5d81eba0ede9bcfe0a26135771c2f0c0f6d36bdc
Mohamed39200Lo <110898938+Mohamed39200Lo@users.noreply.github.com>
2024-07-04 19:49:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
+import telebot
 from threading import Thread
 
-app = Flask('')
+
+bot = telebot.TeleBot("5421940383:AAFbDIqS8cX0taSfUFbuWjlDSqBo1_AfeEk")
+app = Flask(__name__)
 
 @app.route('/')
 def ping():
     return "PONG !, HELLO FROM MTC"
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "مرحبا تم ايقاف البوت موقتا")
+
+@bot.message_handler(func=lambda message: True)
+def reply_to_messages(message):
+    if message.text == "السلام عليكم":
+        bot.reply_to(message, "وعليكم السلام ورحمة الله وبركاته")
+    elif message.text == "كيف الحال":
+        bot.reply_to(message, "الحمد لله بخير")
+    elif message.text == "من أي بلد أنت":
+        bot.reply_to(message, "مصر")
 
 def run():
     app.run(host='0.0.0.0', port=8080)
@@ -13,4 +29,8 @@ def run():
 def server():
     t = Thread(target=run)
     t.start()
-  
+
+if __name__ == "__main__":
+    server()
+    bot.polling()
+    
