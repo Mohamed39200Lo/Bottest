a3760308cdebfb31529c021a7d1f4aba7323f7f0
Mohamed39200Lo <110898938+Mohamed39200Lo@users.noreply.github.com>
2024-09-19 23:16:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask
 import cloudscraper
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-chat_ids = ["-1002037612532", "-1002443500870"] 
+chat_ids = ["-1002039608675", "-1002443500870"] 
 API_TOKEN = '7425541614:AAGhkWzA1uM6QWksUvlUC2slqLGOSSEJvbk'
 bot = telebot.TeleBot(API_TOKEN)
 
